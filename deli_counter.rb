909704ebff832c9b82265d0ca9a69634f84daceb
Chinedu Arinze <c.s.arinze@gmail.com>
2019-03-25 22:38:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(line)
+  if line.size == 0
+    puts "The line is currently empty."
+  else
+    newline = line.collect{|num| "#{num.index + 1}. num"}
+    puts newline.join(" ")
+  end
+end
