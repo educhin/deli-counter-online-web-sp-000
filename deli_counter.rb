6b2e63e20184894137fe04134459bf6c841e672b
Chinedu Arinze <c.s.arinze@gmail.com>
2019-03-25 22:56:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(line)
   if line.size == 0
     puts "The line is currently empty."
   else
-    newline = line.collect{|num| "#{newline.index(num) + 1}. num"}
+    newline = line.collect{|num| "#{line.index(num) + 1}. num"}
     puts "The line is currently: #{newline.join(" ")}"
   end
 end
